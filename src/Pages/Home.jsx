ea876a424bbc699279ce6c6d1108fd87e8f04fce
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-05-11 12:50:43 +0600

    added About section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,39 @@
 import Carousel from "../Components/Carousel/Carousel";
-
+import person from '../assets/images/about_us/person.jpg'
+import parts from '../assets/images/about_us/parts.jpg'
 const Home = () => {
     return (
         <div>
             <Carousel></Carousel>
-            
+            {/* about section start*/}
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col md:flex-row">
+                    <div className="md:w-1/2 relative">
+                        <img src={person} className="w-10/12 rounded-lg mx-auto shadow-2xl" />
+                        <img src={parts} className="max-w-sm border-8 hidden md:block border-white absolute -bottom-16 right-0 rounded-lg shadow-2xl" />
+                    </div>
+                    <div className="md:w-1/2">
+                        <h4 className="text-warning my-3 font-bold text-2xl">About Us</h4>
+                        <h1 className="text-4xl font-bold">We are qualified & of experience in this field</h1>
+                        <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. </p>
+                        <p className="py-6">The majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. </p>
+                        <button className="btn btn-warning">Get More Info</button>
+                    </div>
+                </div>
+            </div>
+            {/* About section end */}
+            {/* Services section start */}
+
+            <div className="mt-40">
+                <div className="text-center mx-auto w-1/2">
+                    <h4 className="text-warning my-3 font-bold text-2xl">Services</h4>
+                    <h1 className="text-4xl font-bold my-3">Our Service Area</h1>
+                    <p className="my-3">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                </div>
+
+            </div>
         </div>
+
     );
 };
 
