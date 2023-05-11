4bf1636e15a4843ed3dd78edf0b470389e5b38fc
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-05-11 11:52:29 +0600

    banner Carousel done

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,3 +1,4 @@
+import bannerImg5 from '../../assets/images/banner/5.jpg'
 import bannerImg1 from '../../assets/images/banner/1.jpg'
 import bannerImg2 from '../../assets/images/banner/2.jpg'
 import bannerImg3 from '../../assets/images/banner/3.jpg'
@@ -5,13 +6,13 @@ import bannerImg4 from '../../assets/images/banner/4.jpg'
 import './Carousel.css'
 const Carousel = () => {
   return (
-    <div className="carousel w-full sm:h-[80vh]">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img src={bannerImg1} className="w-full" />
-        <div className='absolute flex flex-col justify-center items-start left-6  sm:w-[500px] top-[30%]'>
-          <h1 className='text-5xl font-bold banner-title'>Affordable Price <br /> For Car Servicing</h1>
+    <div className="carousel w-full h-screen sm:h-[80vh] rounded-xl ">
+      <div id="slide1" className="carousel-item relative w-full ">
+        <img src={bannerImg5} className="w-full" />
+        <div className='absolute w-full flex flex-col justify-center space-y-6 sm:w-6/12 h-full top-0 bg-gradient-to-r from-[#151515] to-[#15151500] ps-1 sm:ps-14'>
+          <h1 className='text-[9vw] font-bold banner-title sm:text-6xl'>Affordable Price <br /> For Car Servicing</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit blanditiis natus totam expedita quod amet iure, vero unde voluptatum fugiat mollitia incidunt fugit, eveniet animi? Amet necessitatibus accusantium dolore sequi!</p>
-          <div className='flex gap-x-8'>
+          <div className='sm:flex gap-x-8 '>
             <button className="btn btn-warning inline">Discover More</button>
             <button className="btn btn-outline btn-warning inline">Latest Projects</button>
           </div>
@@ -23,11 +24,11 @@ const Carousel = () => {
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img src={bannerImg2} className="w-full" />
-        <div className='absolute flex flex-col justify-center items-start left-6  sm:w-[500px] top-[30%]'>
-          <h1 className='text-5xl font-bold banner-title'>Affordable Price <br /> For Car Servicing</h1>
+        <div className='absolute w-full flex flex-col justify-center space-y-6 sm:w-6/12 h-full top-0 bg-gradient-to-r from-[#151515] to-[#15151500] ps-1 sm:ps-14'>
+          <h1 className='text-[9vw] font-bold banner-title sm:text-6xl'>Affordable Price <br /> For Car Servicing</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit blanditiis natus totam expedita quod amet iure, vero unde voluptatum fugiat mollitia incidunt fugit, eveniet animi? Amet necessitatibus accusantium dolore sequi!</p>
-          <div className='flex gap-x-8'>
-          <button className="btn btn-warning inline">Discover More</button>
+          <div className='sm:flex gap-x-8 '>
+            <button className="btn btn-warning inline">Discover More</button>
             <button className="btn btn-outline btn-warning inline">Latest Projects</button>
           </div>
         </div>
@@ -38,11 +39,11 @@ const Carousel = () => {
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img src={bannerImg3} className="w-full" />
-        <div className='absolute flex flex-col justify-center items-start left-6  sm:w-[500px] top-[30%]'>
-          <h1 className='text-5xl font-bold banner-title'>Affordable Price <br /> For Car Servicing</h1>
+        <div className='absolute w-full flex flex-col justify-center space-y-6 sm:w-6/12 h-full top-0 bg-gradient-to-r from-[#151515] to-[#15151500] ps-1 sm:ps-14'>
+          <h1 className='text-[9vw] font-bold banner-title sm:text-6xl'>Affordable Price <br /> For Car Servicing</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit blanditiis natus totam expedita quod amet iure, vero unde voluptatum fugiat mollitia incidunt fugit, eveniet animi? Amet necessitatibus accusantium dolore sequi!</p>
-          <div className='flex gap-x-8'>
-          <button className="btn btn-warning inline">Discover More</button>
+          <div className='sm:flex gap-x-8 '>
+            <button className="btn btn-warning inline">Discover More</button>
             <button className="btn btn-outline btn-warning inline">Latest Projects</button>
           </div>
         </div>
@@ -53,16 +54,31 @@ const Carousel = () => {
       </div>
       <div id="slide4" className="carousel-item relative w-full">
         <img src={bannerImg4} className="w-full" />
-        <div className='absolute flex flex-col justify-center items-start left-6  sm:w-[500px] top-[30%]'>
-          <h1 className='text-5xl font-bold banner-title'>Affordable Price <br /> For Car Servicing</h1>
+        <div className='absolute w-full flex flex-col justify-center space-y-6 sm:w-6/12 h-full top-0 bg-gradient-to-r from-[#151515] to-[#15151500] ps-1 sm:ps-14'>
+          <h1 className='text-[9vw] font-bold banner-title sm:text-6xl'>Affordable Price <br /> For Car Servicing</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit blanditiis natus totam expedita quod amet iure, vero unde voluptatum fugiat mollitia incidunt fugit, eveniet animi? Amet necessitatibus accusantium dolore sequi!</p>
-          <div className='flex gap-x-8'>
-          <button className="btn btn-warning inline">Discover More</button>
+          <div className='sm:flex gap-x-8 '>
+            <button className="btn btn-warning inline">Discover More</button>
             <button className="btn btn-outline btn-warning inline">Latest Projects</button>
           </div>
         </div>
         <div className="absolute flex gap-x-4 justify-between transform -translate-y-1/2  right-5 bottom-5">
           <a href="#slide3" className=" hover:bg-red-500 border-none btn btn-circle">❮</a>
+          <a href="#slide1" className=" hover:bg-red-500 border-none btn btn-circle">❯</a>
+        </div>
+      </div>
+      <div id="slide5" className="carousel-item relative w-full">
+        <img src={bannerImg1} className="w-full" />
+        <div className='absolute w-full flex flex-col justify-center space-y-6 sm:w-6/12 h-full top-0 bg-gradient-to-r from-[#151515] to-[#15151500] ps-1 sm:ps-14'>
+          <h1 className='text-[9vw] font-bold banner-title sm:text-6xl'>Affordable Price <br /> For Car Servicing</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit blanditiis natus totam expedita quod amet iure, vero unde voluptatum fugiat mollitia incidunt fugit, eveniet animi? Amet necessitatibus accusantium dolore sequi!</p>
+          <div className='sm:flex gap-x-8 '>
+            <button className="btn btn-warning inline">Discover More</button>
+            <button className="btn btn-outline btn-warning inline">Latest Projects</button>
+          </div>
+        </div>
+        <div className="absolute flex gap-x-4 justify-between transform -translate-y-1/2  right-5 bottom-5">
+          <a href="#slide4" className=" hover:bg-red-500 border-none btn btn-circle">❮</a>
           <a href="#slide1" className=" hover:bg-red-500 border-none btn btn-circle">❯</a>
         </div>
       </div>
